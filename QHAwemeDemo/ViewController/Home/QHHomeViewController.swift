4592f03887f2d4d6fc14fbd83fc27bb4c3d52a00
anakinChen <qihuichen@sohu-inc.com>
2017-10-17 15:50:30 +0800

    【Add】首页内容

--- QHAwemeDemo/ViewController/Home/QHHomeViewController.swift
+++ QHAwemeDemo/ViewController/Home/QHHomeViewController.swift
@@ -8,17 +8,74 @@
 
 import UIKit
 
-class QHHomeViewController: QHBaseViewController {
-
+class QHHomeViewController: QHBaseViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let homeCellIdentifier = "QHHomeCellIdentifier"
+    
+    var homeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+    
+    @IBOutlet weak var mainCV: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //设置cell的大小
+        if let layout = mainCV.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height)
+            //滑动方向 默认方向是垂直
+            layout.scrollDirection = .vertical
+            //每个Item之间最小的间距
+            layout.minimumInteritemSpacing = 0
+            //每行之间最小的间距
+            layout.minimumLineSpacing = 0
+        }
+        
+        if #available(iOS 11.0, *) {
+            mainCV.contentInsetAdjustmentBehavior = .never
+        } else {
+            self.automaticallyAdjustsScrollViewInsets = false
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return homeArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: homeCellIdentifier, for: indexPath)
+        cell.backgroundColor = UIColor.randomColor
+        
+        return cell
+    }
+    
+    //MARK: UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
 
+extension UIColor {
+    //返回随机颜色
+    class var randomColor: UIColor {
+        get {
+            let red = CGFloat(arc4random()%256)/255.0
+            let green = CGFloat(arc4random()%256)/255.0
+            let blue = CGFloat(arc4random()%256)/255.0
+            return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        }
+    }
 }
